389a1799b2591943fdf680063d1640074bb4a1db
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-06-02 18:00:27 -0400

    add annotation method for SingleCellExperiment with peak_annoation.tsv

--- R/utils.R
+++ R/utils.R
@@ -11,12 +11,38 @@
 #'
 #' fi <- "~/data/10x/pbmc_3k/pbmc_granulocyte_sorted_3k_atac_peak_annotation.tsv"
 #' pa <- .import_peak_anno(fi)
+#' annotation(sce, name = "peak_annotation") <- pa
 #'
 .import_peak_anno <- function(file) {
     stopifnot(endsWith(file, "peak_annotation.tsv"))
     panno <- readr::read_tsv(file, col_types = c("c", "n", "n", "c", "n", "c"))
     makeGRangesFromDataFrame(panno, keep.extra.columns = TRUE)
 }
+
+.proc_peak_value <- function(value) {
+    if (is.character(value) && file.exists(value))
+        value <- .import_peak_anno(value)
+    value
+}
+
+setReplaceMethod("annotation", "SingleCellExperiment",
+    function(object, ..., value) {
+        args <- list(...)
+        append <- isTRUE(args[["append"]])
+        meta <- metadata(object)
+        annotated <- "annotation" %in% names(meta)
+        if (annotated && !append)
+            warning("'append = FALSE'; replacing annotation in metadata")
+        vlist <- list(.proc_peak_value(value))
+        names(vlist) <- args[["name"]]
+        metadata(object) <- append(meta, list(annotation = vlist))
+        object
+    }
+)
+
+setMethod("annotation", "SingleCellExperiment", function(object, ...) {
+    metadata(object)[["annotation"]]
+})
 
 #' Importing fragment files
 #'
