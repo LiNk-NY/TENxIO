1d05d9dc30d2fcaa294b9b2bf009458cf76b7a53
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-06-03 11:05:10 -0400

    update import fragments

--- R/utils.R
+++ R/utils.R
@@ -11,6 +11,9 @@
 #'
 #' fi <- "~/data/10x/pbmc_3k/pbmc_granulocyte_sorted_3k_atac_peak_annotation.tsv"
 #' pa <- .import_peak_anno(fi)
+#'
+#' con <- TENxFile("~/data/10x/pbmc_3k/pbmc_granulocyte_sorted_3k_filtered_feature_bc_matrix.h5")
+#' sce <- import(con)
 #' annotation(sce, name = "peak_annotation") <- pa
 #'
 .import_peak_anno <- function(file) {
@@ -33,8 +36,9 @@ setReplaceMethod("annotation", "SingleCellExperiment",
         annotated <- "annotation" %in% names(meta)
         if (annotated && !append)
             warning("'append = FALSE'; replacing annotation in metadata")
-        vlist <- list(.proc_peak_value(value))
-        names(vlist) <- args[["name"]]
+        vlist <- structure(
+            list(.proc_peak_value(value)), .Names = args[["name"]]
+        )
         metadata(object) <- append(meta, list(annotation = vlist))
         object
     }
@@ -48,32 +52,65 @@ setMethod("annotation", "SingleCellExperiment", function(object, ...) {
 #'
 #' @keywords internal
 #'
+#' @param file character(1) The file path to the fragments resource, usually a
+#'   compressed tabix file with extension `.tsv.gz`.
+#'
+#' @param roi GRanges() A GRanges indicating the regions of interest. This
+#'   get sent to `RSamtools` as the `param` input.
+#'
+#' @param yieldSize numeric() The number of records to read by default, 200
+#'   records will be imported. A warning will be emmitted if not modified.
+#'
+#' @param index character(1) The index file usually ending in `.tbi`. It
+#'   should be found in the same location as the `file` input
+#'
+#' @importFrom GenomicRanges GRanges makeGRangesFromDataFrame
+#' @importFrom S4Vectors splitAsList mcols
+#'
 #' @examples
 #'
 #' con <- TENxFile("~/data/10x/pbmc_3k/pbmc_granulocyte_sorted_3k_filtered_feature_bc_matrix.h5")
 #' sce <- import(con)
+#'
 #' fr <- "~/data/10x/pbmc_3k/pbmc_granulocyte_sorted_3k_atac_fragments.tsv.gz"
 #' fra <- .import_fragments(fr)
 #'
-.import_fragments <- function(file, withIndex = TRUE) {
-    ## read tabix format with Rsamtools
-    stopifnot(endsWith(file, "atac_fragments.tsv.gz"))
-    tabindex <- paste0(file, ".tbi")
-    if (withIndex)
-        stopifnot(file.exists(tabindex))
+.import_fragments <-
+    function(
+        file, roi = GRanges(), yieldSize = 200, index = paste0(file, ".tbi")
+    )
+{
+    .checkPkgsAvail(c("Rsamtools", "RaggedExperiment"))
+    if (!endsWith(file, "atac_fragments.tsv.gz"))
+        stop("Provide a 10X fragments file ending in 'atac_fragments.tsv.gz'")
 
-    tb <- Rsamtools::TabixFile(file, tabindex, yieldSize = NA_integer_)
-    # reslist <- scanTabix(tb, param = rowRanges(sce))
-    # lapply(Filter(length, reslist), function(x) read.table(textConnection(x)))
-    ex <- read.table(textConnection(scanTabix(tb)[[1]]))
+    if (!file.exists(index))
+        stop("Tabix file index not found")
 
-    ## names from table in the link below
-    ## https://support.10xgenomics.com/single-cell-atac/software/pipelines/latest/output/fragments
+    if (missing(yieldSize))
+        warning("Using default 'yieldSize' parameter")
+    tb <- Rsamtools::TabixFile(file, tabindex, yieldSize = yieldSize)
+    ex <- read.table(
+        textConnection(Rsamtools::scanTabix(tb, param = roi)[[1]])
+    )
+    ## https://support.10xgenomics.com/single-cell-atac/
+    ## software/pipelines/latest/output/fragments
     names(ex) <- c("chrom", "chromStart", "chromEnd", "barcode", "readSupport")
     ggr <- makeGRangesFromDataFrame(
         ex, keep.extra.columns = TRUE, starts.in.df.are.0based = TRUE
     )
-    ggrl <- as(splitAsList(ggr, mcols(ggr)$barcode), "GRangesList")
-    RaggedExperiment(ggrl)
-    # compactAssay(erg, i = "readSupport", sparse = TRUE)
+    grs <- splitAsList(ggr, mcols(ggr)$barcode)
+    ggrl <- as(grs, "GRangesList")
+    RaggedExperiment::RaggedExperiment(ggrl)
+}
+
+.checkPkgsAvail <- function(pkgnames) {
+    vapply(pkgnames, function(pkgname) {
+        func <- as.character(sys.call(-3L)[[1L]])
+        func <- tail(func, 1L)
+        if (!requireNamespace(pkgname, quietly = TRUE))
+            stop("Install '", pkgname, "' to use '", func, "'", call. = FALSE)
+        else
+            TRUE
+    }, logical(1L))
 }
