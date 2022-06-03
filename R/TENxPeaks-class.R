af8481b5c8020227b8a432bfa4a32bb39b9ef40e
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-06-03 18:44:34 -0400

    update peaks import

---
+++ R/TENxPeaks-class.R
@@ -0,0 +1,67 @@
+#' @exportClass TENxPeaks
+.TENxPeaks <- setClass(
+    Class = "TENxPeaks",
+    contains = "TENxFile"
+)
+
+.check_peaks <- function(object) {
+    fpath <- path(object)
+    if (!endsWith(fpath, "peak_annotation.tsv"))
+        "Provide a 10X Peaks file ending in 'peak_annotation.tsv'"
+    else
+        TRUE
+}
+
+.validPeaksFile <- function(object) {
+    .check_peaks(object)
+}
+
+S4Vectors::setValidity2("TENxPeaks", .validPeaksFile)
+
+#' @rdname TENxPeaks
+#'
+#' @title Importing peak-annotation files
+#'
+#' @examples
+#'
+#' fi <- "~/data/10x/pbmc_3k/pbmc_granulocyte_sorted_3k_atac_peak_annotation.tsv"
+#' pa <- TENxPeaks(fi)
+#' import(pa)
+#'
+#' con <- TENxFile("~/data/10x/pbmc_3k/pbmc_granulocyte_sorted_3k_filtered_feature_bc_matrix.h5")
+#' sce <- import(con)
+#' annotation(sce, name = "peak_annotation") <- pa
+#'
+TENxPeaks <- function(resource, ...) {
+    .TENxPeaks(resource = resource, ...)
+}
+
+setMethod("import", "TENxPeaks", function(con, format, ...) {
+    .checkPkgsAvail("readr")
+    panno <- readr::read_tsv(
+        file = path(con), col_types = c("c", "n", "n", "c", "n", "c")
+    )
+    makeGRangesFromDataFrame(panno, keep.extra.columns = TRUE)
+})
+
+setReplaceMethod("annotation", "SingleCellExperiment",
+    function(object, ..., value) {
+        if (!is(value, "TENxPeaks"))
+            stop("'value' must be of class 'TENxPeaks'")
+        args <- list(...)
+        append <- isTRUE(args[["append"]])
+        meta <- metadata(object)
+        annotated <- "annotation" %in% names(meta)
+        if (annotated && !append)
+            warning("'append = FALSE'; replacing annotation in metadata")
+        vlist <- structure(
+            list(import(value)), .Names = args[["name"]]
+        )
+        metadata(object) <- append(meta, list(annotation = vlist))
+        object
+    }
+)
+
+setMethod("annotation", "SingleCellExperiment", function(object, ...) {
+    metadata(object)[["annotation"]]
+})
--- R/utils.R
+++ R/utils.R
@@ -3,54 +3,6 @@
     object
 }
 
-#' Importing peak-annotation files
-#'
-#' @keywords internal
-#'
-#' @examples
-#'
-#' fi <- "~/data/10x/pbmc_3k/pbmc_granulocyte_sorted_3k_atac_peak_annotation.tsv"
-#' pa <- .import_peak_anno(fi)
-#'
-#' con <- TENxFile("~/data/10x/pbmc_3k/pbmc_granulocyte_sorted_3k_filtered_feature_bc_matrix.h5")
-#' sce <- import(con)
-#' annotation(sce, name = "peak_annotation") <- pa
-#'
-.import_peak_anno <- function(file) {
-    stopifnot(endsWith(file, "peak_annotation.tsv"))
-    .checkPkgsAvail("readr")
-    panno <- readr::read_tsv(
-        file = file, col_types = c("c", "n", "n", "c", "n", "c")
-    )
-    makeGRangesFromDataFrame(panno, keep.extra.columns = TRUE)
-}
-
-.proc_peak_value <- function(value) {
-    if (is.character(value) && file.exists(value))
-        value <- .import_peak_anno(value)
-    value
-}
-
-setReplaceMethod("annotation", "SingleCellExperiment",
-    function(object, ..., value) {
-        args <- list(...)
-        append <- isTRUE(args[["append"]])
-        meta <- metadata(object)
-        annotated <- "annotation" %in% names(meta)
-        if (annotated && !append)
-            warning("'append = FALSE'; replacing annotation in metadata")
-        vlist <- structure(
-            list(.proc_peak_value(value)), .Names = args[["name"]]
-        )
-        metadata(object) <- append(meta, list(annotation = vlist))
-        object
-    }
-)
-
-setMethod("annotation", "SingleCellExperiment", function(object, ...) {
-    metadata(object)[["annotation"]]
-})
-
 .checkPkgsAvail <- function(pkgnames, walkback = -3L) {
     vapply(pkgnames, function(pkgname) {
         func <- as.character(sys.call(walkback)[[1L]])
