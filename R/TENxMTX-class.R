90af7a786e6ce65c968b0afab5edf1c18c29f226
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-06-22 14:09:10 -0400

    update TENxMTX constructor

--- R/TENxMTX-class.R
+++ R/TENxMTX-class.R
@@ -1,13 +1,36 @@
+#' TENxMTX: The Matrix Market representation class for 10X Data
+#'
+#'
 .TENxMTX <- setClass(
     Class = "TENxMTX",
-    contains = "TENxFile"
+    contains = "TENxFile",
+    slots = c(compressed = "logical")
 )
+
+#' @examples
+#'
+#' mtxf <-"~/data/10x/pbmc_3k/filtered_feature_bc_matrix/matrix.mtx.gz"
+#' con <- TENxMTX(mtxf)
+#' import(con)
+#'
+#' @export
+TENxMTX <- function(resource, compressed = FALSE, ...) {
+    dots <- list(...)
+    ext <- dots[["extension"]]
+    if (is.null(ext))
+        ext <- .get_ext(resource)
+    compr <- identical(ext, "mtx.gz")
+    if (!ext %in% c("mtx.gz", "mtx"))
+        warning("File extension is not 'mtx'; import may fail", call. = FALSE)
+    .TENxMTX(resource = resource, compressed = compr, extension = ext)
+}
 
 #' @import SummarizedExperiment
 #' @export
 setMethod("import", "TENxMTX", function(con, format, text, ...) {
-    mtxf <- SingleCellMultiModal:::.read_mtx(path(con))
-    ## TODO: make use of other files
+    mtxf <- Matrix::readMM(path(con))
+    ## coerce to common use class
+    mtxf <- as(mtxf, "dgCMatrix")
     SummarizedExperiment::SummarizedExperiment(
         assays = SimpleList(counts = mtxf)
     )
