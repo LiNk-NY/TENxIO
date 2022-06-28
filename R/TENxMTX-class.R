990c3a2fd26aaa91fdac677f67ec21685c3dda73
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-06-28 12:47:36 -0400

    export MTX class and update docs

--- R/TENxMTX-class.R
+++ R/TENxMTX-class.R
@@ -1,12 +1,40 @@
 #' TENxMTX: The Matrix Market representation class for 10X Data
 #'
+#' @description This class is designed to work with 10x MTX datasets,
+#'   particularly from the multiome pipelines.
 #'
+#' @slot compressed logical(1) Whether or not the file is in compressed format,
+#'   usually gzipped (`.gz`).
+#'
+#' @details The `TENxMTX` class is a straightforward implementation that allows
+#'   the user to import a Matrix Market file format using `Matrix::readMM`.
+#'   Currently, it only supports return types of `dgCMatrix`. To request other
+#'   formats, please open an issue on GitHub.
+#'
+#' @exportClass TENxMTX
 .TENxMTX <- setClass(
     Class = "TENxMTX",
     contains = "TENxFile",
     slots = c(compressed = "logical")
 )
 
+#' TENxMTX: Represent Matrix Market Format Files from 10X
+#'
+#' This constructor function accepts `.mtx` and `.mtx.gz` compressed formats
+#' for eventual importing. It is mainly used with tarball files from 10X
+#' Genomics, where more annotation data is included. Importing solely the
+#' `.mtx` format will provide users with a sparse matrix of `dgCMatrix` class
+#' from the `Matrix` package. Currently, other formats are not supported but
+#' if you'd like to request support for a format, please open an issue on
+#' GitHub.
+#'
+#' @inheritParams TENxFile
+#'
+#' @param compressed logical(1) Whether the resource file is compressed (default
+#'   FALSE)
+#'
+#' @return An instance of the `TENxMTX` class
+#'
 #' @examples
 #'
 #' mtxf <-"~/data/10x/pbmc_3k/filtered_feature_bc_matrix/matrix.mtx.gz"
