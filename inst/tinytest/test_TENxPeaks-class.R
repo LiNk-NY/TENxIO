7ffffac6cb31a522d021b2b5de897e551a917158
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-08-16 12:46:47 -0400

    update TENxPeaks unit tests

--- inst/tinytest/test_TENxPeaks-class.R
+++ inst/tinytest/test_TENxPeaks-class.R
@@ -26,3 +26,45 @@ expect_identical(
 expect_identical(
     colnames(mcols(peak_anno)), c("gene", "distance", "peak_type")
 )
+
+sce <- SingleCellExperiment::SingleCellExperiment(
+    assays = SimpleList(counts = matrix(rnorm(100), 10, 10))
+)
+
+expect_error(
+    annotation(sce, name = "peak_annotation") <- peak_anno
+)
+
+expect_silent(
+    annotation(sce, name = "peak_annotation") <- peak_file
+)
+
+expect_identical(
+    metadata(sce), list(annotation = list(peak_annotation = peak_anno))
+)
+
+expect_identical(
+    names(annotation(sce)), "peak_annotation"
+)
+
+expect_warning(
+    annotation(sce, append = FALSE) <- peak_file
+)
+
+expect_silent(
+    annotation(sce, append = TRUE) <- peak_file
+)
+
+expect_identical(
+    length(metadata(sce)), 1L
+)
+
+expect_identical(
+    length(metadata(sce)[["annotation"]]), 2L
+)
+
+expect_identical(
+    names(metadata(sce)[["annotation"]]),
+    c("peak_annotation", "peak_annotation")
+)
+
