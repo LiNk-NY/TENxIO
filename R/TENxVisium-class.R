fe4a4317b700e63b290275247ee2cdfd4779a3dc
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-10-20 12:45:29 -0400

    avoid partial argument match

--- R/TENxVisium-class.R
+++ R/TENxVisium-class.R
@@ -166,7 +166,7 @@ TENxVisium <- function(
     } else {
         resources <- TENxFileList(resources, ...)
         spatialResource <- TENxSpatialList(
-            resource = spatialResource, sample_id = sample_id, images = images,
+            resources = spatialResource, sample_id = sample_id, images = images,
             jsonFile = jsonFile, tissuePattern = tissuePattern
         )
     }
